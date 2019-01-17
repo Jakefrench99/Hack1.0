f1c4b9fb2365d68f4dcf694fd6a465cdd8eba051
Jakefrench99 <Jakefrench99@users.noreply.github.com>
2019-01-17 13:07:11 -0600

    I created a hello world program
    It prints my name and major

---
+++ hello.c
@@ -0,0 +1,19 @@
+/*
+*	Author: Jacob French and Bethany Krull
+*	Date: 2019/17/01
+*	
+*	A program that prints my name
+*
+*/
+
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+	printf("Jacob French\n"); //prints my name 
+	printf("Electrical Engineering\n");
+	
+	return 0;
+
+}
